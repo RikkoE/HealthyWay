936ccb80d7cd2b608537ccd6706eb42afe0f93e4
Rikko <rickard.e.1992@gmail.com>
2015-04-01 17:27:48 +0200

    gui update

--- AndroidStudioProjects/HealthyWay/app/src/main/java/com/example/rikko/healthyway/MainActivity.java
+++ AndroidStudioProjects/HealthyWay/app/src/main/java/com/example/rikko/healthyway/MainActivity.java
@@ -12,9 +12,11 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends Activity {
@@ -26,15 +28,19 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView out = (TextView) findViewById(R.id.out);
-        final Button turnON = (Button) findViewById(R.id.turnON);
-        final Button discoverable = (Button) findViewById(R.id.discoverable);
+
+        //final Button turnON = (Button) findViewById(R.id.turnON);
         final Button scan = (Button) findViewById(R.id.scan);
-        final Button turnOFF = (Button) findViewById(R.id.turnOFF);
-        final Button device = (Button) findViewById(R.id.device);
+        //final Button turnOFF = (Button) findViewById(R.id.turnOFF);
+
+        final ToggleButton toggleBlue = (ToggleButton) findViewById(R.id.toggleBluetooth);
+
         final BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
         btArrayAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1);
         final ListView device_list = (ListView) findViewById(R.id.devicesfound);
         device_list.setAdapter(btArrayAdapter);
+
+        boolean btOn = false;
 
         final BroadcastReceiver mReceiver = new BroadcastReceiver() {
             public void onReceive(Context context, Intent intent) {
@@ -43,7 +49,7 @@ public class MainActivity extends Activity {
                 if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
                     //discovery starts, we can show progress dialog or perform other tasks
                 } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                    //discovery finishes, dismis progress dialog
+                    //discovery finishes, dismiss progress dialog
                 } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                     //bluetooth device found
                     BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
@@ -55,8 +61,35 @@ public class MainActivity extends Activity {
             }
         };
 
+        btOn = bluetooth.isEnabled();
 
-        turnON.setOnClickListener(new View.OnClickListener() {
+        if (!btOn) {
+            toggleBlue.setChecked(false);
+        } else {
+            toggleBlue.setChecked(true);
+        }
+
+        toggleBlue.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    if (!bluetooth.isEnabled()) {
+                        Toast.makeText(getApplicationContext(),
+                                "Turning on Bluetooth", Toast.LENGTH_LONG).show();
+                        // Intent enableBtIntent = new
+                        // Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(new Intent(
+                                BluetoothAdapter.ACTION_REQUEST_ENABLE), 0);
+                    }
+                } else {
+                    btArrayAdapter.clear();
+                    bluetooth.disable();
+                    Toast.makeText(getApplicationContext(),
+                            "Turning off Bluetooth", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        /*turnON.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (!bluetooth.isEnabled()) {
                     Toast.makeText(getApplicationContext(),
@@ -68,40 +101,6 @@ public class MainActivity extends Activity {
                 }
             }
         });
-        discoverable.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                if (!bluetooth.isDiscovering()) {
-                    Toast.makeText(getApplicationContext(),
-                            "MAKING YOUR DEVICE DISCOVERABLE",Toast.LENGTH_LONG);
-                    // Intent enableBtIntent = new
-                    // Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                    startActivityForResult(new Intent(
-                            BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE), 0);
-
-                }
-            }
-        });
-        scan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                btArrayAdapter.clear();
-                IntentFilter filter = new IntentFilter();
-                // String address=bluetooth.getAddress();
-                Toast.makeText(getApplicationContext(),
-                        "Starting scan...: " + bluetooth.startDiscovery(), Toast.LENGTH_LONG).show();
-
-                filter.addAction(BluetoothDevice.ACTION_FOUND);
-                filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-                filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-
-                registerReceiver(mReceiver, filter);
-
-                // Intent enableBtIntent = new
-                // Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-            }
-        });
-
 
         turnOFF.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -112,6 +111,30 @@ public class MainActivity extends Activity {
                         "TURNING OFF BLUETOOTH", Toast.LENGTH_LONG);
             }
         });
+        */
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (bluetooth.isEnabled()) {
+                    btArrayAdapter.clear();
+                    IntentFilter filter = new IntentFilter();
+                    Toast.makeText(getApplicationContext(),
+                            "Starting scan...", Toast.LENGTH_LONG).show();
+                    bluetooth.startDiscovery();
+
+                    filter.addAction(BluetoothDevice.ACTION_FOUND);
+                    filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+                    filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+                    registerReceiver(mReceiver, filter);
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Turn on Bluetooth to scan", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 
     @Override
